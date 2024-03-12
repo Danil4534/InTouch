71f90c627a8d531f037ae39822091d0c2ed700d9
DanilTsapok <daniltsapok12012004@gmail.com>
2024-03-12 19:56:37 +0300

    12.03.2024 Bugfix

--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from "react";
 import style from "./scrollUp.module.scss";
 import ArrowIcon from "../../assets/svg/Up.svg";
+import { positions } from "@mui/system";
+
 function ScrollUp() {
   const [activeScrollBtn, setActiveScrollBtn] = useState(false);
-  const handleScroll = () => {
-    const position = window.scrollY;
-    console.log(position);
-    if (position >= 300) {
-      setActiveScrollBtn(true);
-    } else if (position <= 100) {
-      setActiveScrollBtn(false);
-    }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+
+      position >= 300 ? setActiveScrollBtn(true) : setActiveScrollBtn(false);
+      //   if (position >= 300) {
+      //     console.log(position + "Test2");
+      //     setActiveScrollBtn(true);
+      //   } else if (position <= 100) {
+      //     setActiveScrollBtn(false);
+      //     console.log(position + "Test3");
+      //   }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
-
-  window.addEventListener("scroll", handleScroll);
-
   return (
     <>
       {activeScrollBtn ? (
         <div className={style.btnScroll}>
-          <a href="#">
+          <button onClick={() => scrollToTop()}>
             <img src={ArrowIcon} alt="" />
-          </a>
+          </button>
         </div>
-      ) : null}
+      ) : (
+        <></>
+      )}
     </>
   );
 }
